6d776d0799df21030198cedc40df92991c2711f1
BreyMene <menesesmunozfelipe@gmail.com>
2024-10-17 18:54:44 -0500

    17/10

---
+++ asp_servicios/Startup.cs
@@ -0,0 +1,53 @@
+﻿using asp_servicios.Controllers;
+using lib_aplicaciones.Implementaciones;
+using lib_aplicaciones.Interfaces;
+using lib_repositorios;
+using lib_repositorios.Implementaciones;
+using lib_repositorios.Interfaces;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace asp_servicios
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration? Configuration { set; get; }
+
+        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        {
+            services.Configure<KestrelServerOptions>(x => { x.AllowSynchronousIO = true; });
+            services.Configure<IISServerOptions>(x => { x.AllowSynchronousIO = true; });
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddScoped<Conexion, Conexion>();
+            // Repositorios
+            services.AddScoped<IBarriosRepositorio, BarriosRepositorio>();
+            // Aplicaciones
+            services.AddScoped<IBarriosAplicacion, BarriosAplicacion>();
+            // Controladores
+            services.AddScoped<TokenController, TokenController>();
+
+            services.AddCors(o => o.AddDefaultPolicy(b => b.AllowAnyOrigin()));
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+            app.UseRouting();
+            app.UseCors();
+        }
+    }
+}
--- lib_utilidades/DatosGenerales.cs
+++ lib_utilidades/DatosGenerales.cs
@@ -2,7 +2,7 @@
 {
     public class DatosGenerales
     {
-        public static string ruta_json = @"E:\notas\secrets.json";
+        public static string ruta_json = @"E:\Barrios\secrets.json";
         public static bool usa_azure = false;
         public static string clave = "EVBgi345936456ghhVBJGtgnifytsidi3456678jhgUTytutyiiyi";
         public static string usuario_datos = EncriptarConversor.Encriptar("Test.Trghhjsgdj");
