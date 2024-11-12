dd7dc2090b0a4604ada6c918316405cffdc33ee8
BreyMene <menesesmunozfelipe@gmail.com>
2024-11-11 20:48:26 -0500

    lib_presentaciones

---
+++ lib_presentaciones/Implementaciones/BarriosPresentacion.cs
@@ -0,0 +1,109 @@
+﻿using lib_comunicaciones.Interfaces;
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+
+namespace lib_presentaciones.Implementaciones
+{
+    public class BarriosPresentacion : IBarriosPresentacion
+    {
+        private IBarriosComunicacion? iComunicacion = null;
+
+        public BarriosPresentacion(IBarriosComunicacion iComunicacion)
+        {
+            this.iComunicacion = iComunicacion;
+        }
+
+        public async Task<List<Barrios>> Listar()
+        {
+            var lista = new List<Barrios>();
+            var datos = new Dictionary<string, object>();
+
+            var respuesta = await iComunicacion!.Listar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Barrios>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<List<Barrios>> Buscar(Barrios entidad, string tipo)
+        {
+            var lista = new List<Barrios>();
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+            datos["Tipo"] = tipo;
+
+            var respuesta = await iComunicacion!.Buscar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            lista = JsonConversor.ConvertirAObjeto<List<Barrios>>(
+                JsonConversor.ConvertirAString(respuesta["Entidades"]));
+            return lista;
+        }
+
+        public async Task<Barrios> Guardar(Barrios entidad)
+        {
+            if (entidad.Id != 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Guardar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Barrios>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Barrios> Modificar(Barrios entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Modificar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Barrios>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+
+        public async Task<Barrios> Borrar(Barrios entidad)
+        {
+            if (entidad.Id == 0 || !entidad.Validar())
+            {
+                throw new Exception("lbFaltaInformacion");
+            }
+
+            var datos = new Dictionary<string, object>();
+            datos["Entidad"] = entidad;
+
+            var respuesta = await iComunicacion!.Borrar(datos);
+            if (respuesta.ContainsKey("Error"))
+            {
+                throw new Exception(respuesta["Error"].ToString()!);
+            }
+            entidad = JsonConversor.ConvertirAObjeto<Barrios>(
+                JsonConversor.ConvertirAString(respuesta["Entidad"]));
+            return entidad;
+        }
+    }
+}
---
+++ lib_presentaciones/Interfaces/IBarriosPresentacion.cs
@@ -0,0 +1,13 @@
+﻿using lib_entidades.Modelos;
+
+namespace lib_presentaciones.Interfaces
+{
+    public interface IBarriosPresentacion
+    {
+        Task<List<Barrios>> Listar();
+        Task<List<Barrios>> Buscar(Barrios entidad, string tipo);
+        Task<Barrios> Guardar(Barrios entidad);
+        Task<Barrios> Modificar(Barrios entidad);
+        Task<Barrios> Borrar(Barrios entidad);
+    }
+}
