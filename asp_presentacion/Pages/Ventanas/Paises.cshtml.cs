96cb22b8fbc68220a14108b224a36035351fef7d
BreyMene <menesesmunozfelipe@gmail.com>
2024-11-20 21:55:52 -0500

    Añade asp_servicios con Paises

---
+++ asp_presentacion/Pages/Ventanas/Paises.cshtml.cs
@@ -0,0 +1,161 @@
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace asp_presentacion.Pages.Ventanas
+{
+    public class PaisesModel : PageModel
+    {
+        private IPaisesPresentacion? iPresentacion = null;
+
+        public PaisesModel(IPaisesPresentacion iPresentacion)
+        {
+            try
+            {
+                this.iPresentacion = iPresentacion;
+                Filtro = new Paises();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        [BindProperty] public Enumerables.Ventanas Accion { get; set; }
+        [BindProperty] public Paises? Actual { get; set; }
+        [BindProperty] public Paises? Filtro { get; set; }
+        [BindProperty] public List<Paises>? Lista { get; set; }
+
+        public virtual void OnGet() { OnPostBtRefrescar(); }
+
+        public void OnPostBtRefrescar()
+        {
+            try
+            {
+                var variable_session = HttpContext.Session.GetString("key");
+                if (String.IsNullOrEmpty(variable_session))
+                    HttpContext.Session.SetString("key", "Pruebas");
+
+                Filtro!.Nombre = Filtro!.Nombre ?? "";
+
+                Accion = Enumerables.Ventanas.Listas;
+                var task = this.iPresentacion!.Buscar(Filtro!, "NOMBRE");
+                task.Wait();
+                Lista = task.Result;
+                Actual = null;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtNuevo()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = new Paises()
+                {
+                    Nombre = "Mexico"
+                };
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtModificar(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Editar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtGuardar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Editar;
+                Task<Paises>? task = null;
+                if (Actual!.Id == 0)
+                    task = this.iPresentacion!.Guardar(Actual!);
+                else
+                    task = this.iPresentacion!.Modificar(Actual!);
+                task.Wait();
+                Actual = task.Result;
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrarVal(string data)
+        {
+            try
+            {
+                OnPostBtRefrescar();
+                Accion = Enumerables.Ventanas.Borrar;
+                Actual = Lista!.FirstOrDefault(x => x.Id.ToString() == data);
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public virtual void OnPostBtBorrar()
+        {
+            try
+            {
+                var task = this.iPresentacion!.Borrar(Actual!);
+                Actual = task.Result;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtCancelar()
+        {
+            try
+            {
+                Accion = Enumerables.Ventanas.Listas;
+                OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtCerrar()
+        {
+            try
+            {
+                if (Accion == Enumerables.Ventanas.Listas)
+                    OnPostBtRefrescar();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+    }
+}
---
+++ asp_presentacion/Startup.cs
@@ -0,0 +1,47 @@
+﻿using lib_comunicaciones.Implementaciones;
+using lib_comunicaciones.Interfaces;
+using lib_presentaciones.Implementaciones;
+using lib_presentaciones.Interfaces;
+
+namespace asp_presentacion
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public static IConfiguration? Configuration { set; get; }
+
+        public void ConfigureServices(WebApplicationBuilder builder, IServiceCollection services)
+        {
+            // Comunicaciones
+            services.AddScoped<IPaisesComunicacion, PaisesComunicacion>();
+            // Presentaciones
+            services.AddScoped<IPaisesPresentacion, PaisesPresentacion>();
+
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddRazorPages();
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromMinutes(30);
+            });
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment env)
+        {
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+            }
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthorization();
+            app.MapRazorPages();
+            app.UseSession();
+            app.Run();
+        }
+    }
+}
