b30ca2e7d7920a1adef1fe6e88c7e7964668fe4c
Memesis23 <miguelcraft677lol@gmail.com>
2024-11-24 18:30:00 -0500

    #auditoria.1

---
+++ lib_entidades/Modelos/Auditorias.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace lib_entidades.Modelos
+{
+    internal class Auditorias
+    {
+        [Key] public int Id { get; set; } //Id de Auditoria
+        public string? Tabla { get; set; } //Nombre de la Tabla
+        public string? Referencia { get; set; } //Nombre de la Tabla
+        public string? Accion { get; set; } //Accion del metodo
+        public DateTime? Fecha { get; set; } //Fecha de la Auditoria
+    }
+}
