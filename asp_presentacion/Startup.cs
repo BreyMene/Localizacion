c8232a066c1cbbf0dece3bac61640b46c0d0c938
SantiUsuga <santiagocorrea331182@correo.itm.edu.co>
2024-11-25 15:13:12 -0500

    Boton borrar arreglado, faltan los detalles y hacer que las coordenadas de ubicaciones se muestren ambos atributos a la hora de editar

--- asp_presentacion/Startup.cs
+++ asp_presentacion/Startup.cs
@@ -24,6 +24,7 @@ namespace asp_presentacion
             services.AddScoped<IUsuariosComunicacion, UsuariosComunicacion>();
             services.AddScoped<IRolesComunicacion, RolesComunicacion>();
             services.AddScoped<ICoordenadasComunicacion, CoordenadasComunicacion>();
+            services.AddScoped<IUbicacionesComunicacion, UbicacionesComunicacion>();
             // Presentaciones
             services.AddScoped<IPaisesPresentacion, PaisesPresentacion>();
             services.AddScoped<ICoordenadasPresentacion, CoordenadasPresentacion>();
@@ -32,8 +33,9 @@ namespace asp_presentacion
             services.AddScoped<IDepartamentosPresentacion, DepartamentosPresentacion>();
             services.AddScoped<ICiudadesPresentacion, CiudadesPresentacion>();
 			services.AddScoped<IBarriosPresentacion, BarriosPresentacion>();
+            services.AddScoped<IUbicacionesPresentacion, UbicacionesPresentacion>();
 
-			services.AddControllers();
+            services.AddControllers();
             services.AddEndpointsApiExplorer();
             services.AddRazorPages();
             services.AddSession(options =>
