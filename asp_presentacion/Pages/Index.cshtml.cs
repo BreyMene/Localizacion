4c0772e3760b934642a8190cf2780110a1c5ed9f
Andreuxbot <andrexbot@gmail.com>
2024-11-28 01:01:46 -0500

    secion funcional, falta el que oculte

--- asp_presentacion/Pages/Index.cshtml.cs
+++ asp_presentacion/Pages/Index.cshtml.cs
@@ -1,20 +1,124 @@
+using lib_entidades.Modelos;
+using lib_presentaciones.Interfaces;
+using lib_utilidades;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Razor.TagHelpers;
 
 namespace asp_presentacion.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private IUsuariosPresentacion? iUsuariosPresentacion = null;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(IUsuariosPresentacion iUsuariosPresentacion)
         {
-            _logger = logger;
+            try
+            {
+                this.iUsuariosPresentacion = iUsuariosPresentacion;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
         }
+
+        [BindProperty] public List<Usuarios>? Usuarios { get; set; }
+
+        public bool EstaLogueado = false;
+        [BindProperty] public string? Email { get; set; }
+        [BindProperty] public string? Contraseña { get; set; }
 
         public void OnGet()
         {
+            var variable_session = HttpContext.Session.GetString("Usuario");
+            if (!String.IsNullOrEmpty(variable_session))
+            {
+                EstaLogueado = true;
+                return;
+            }
+        }
 
+        public void OnPostBtClean()
+        {
+            try
+            {
+                Email = string.Empty;
+                Contraseña = string.Empty;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public void CargarUsuarios()
+        {
+            try
+            {
+                if (!(Usuarios == null || Usuarios!.Count <= 0))
+                    return;
+
+                var task = this.iUsuariosPresentacion!.Listar();
+                task.Wait();
+                Usuarios = task.Result;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+        public string ConvertirUsuario(string Nombre)
+        {
+            try
+            {
+                CargarUsuarios();
+                return Usuarios!.FirstOrDefault(x => x.Nombre == Nombre)!.Contrasena!;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+                return string.Empty;
+            }
+        }
+        public void OnPostBtEnter()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(Email) &&
+                    string.IsNullOrEmpty(Contraseña))
+                {
+                    OnPostBtClean();
+                    return;
+                }
+                CargarUsuarios();
+                if (ConvertirUsuario(Email) != Contraseña)
+                {
+                    OnPostBtClean();
+                    return;
+                }
+                ViewData["Logged"] = true;
+                HttpContext.Session.SetString("Usuario", Email!);
+                EstaLogueado = true;
+                OnPostBtClean();
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
+        }
+
+        public void OnPostBtClose()
+        {
+            try
+            {
+                HttpContext.Session.Clear();
+                HttpContext.Response.Redirect("/");
+                EstaLogueado = false;
+            }
+            catch (Exception ex)
+            {
+                LogConversor.Log(ex, ViewData!);
+            }
         }
     }
 }
