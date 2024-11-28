d17eb52ec939984457afe8c0893ac7d7cce67181
Memesis23 <miguelcraft677lol@gmail.com>
2024-11-27 21:11:26 -0500

    Auditorias presentacion

---
+++ Comunicaciones/Interfaces/IAuditoriasComunicacion.cs
@@ -0,0 +1,8 @@
+﻿namespace lib_comunicaciones.Interfaces
+{
+    public interface IAuditoriasComunicacion
+    {
+        Task<Dictionary<string, object>> Listar(Dictionary<string, object> datos);
+        Task<Dictionary<string, object>> Buscar(Dictionary<string, object> datos);
+    }
+}
